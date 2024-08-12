0c5e2c50bd195edd23cbe778b1ad6f90107363f4
Brenno Alencar <127119853+BrennoAlenkar@users.noreply.github.com>
2024-08-12 16:48:09 -0300

    Atualizei

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
-const ajax = new XMLHttpRequest();
-ajax.open('GET', 'https://viacep.com.br/ws/01001000/json/');
-ajax.send();
+function buscarCEP() {
+    let input = document.getElementById('input').value;
+    const ajax = new XMLHttpRequest();
+    ajax.open('GET', 'https://viacep.com.br/ws/' + input + '/json/');
+    ajax.send();
 
-ajax.onload = function() {
-    document.querySelector('.teste').innerHTML = this.responseText
-}
+    ajax.onload = function () {
+        document.querySelector('.teste').innerHTML = this.responseText;
+
+        let obj = JSON.parse(this.responseText);
+
+        let cidade = 
+    }
+} 
